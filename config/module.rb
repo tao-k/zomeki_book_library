70a537bd58b742a0adfdf15186c8744e5c0a412f
tao-k <tao@sitebridge.co.jp>
2017-12-04 16:24:46 +0900

    config

---
+++ config/module.rb
@@ -0,0 +1,10 @@
+Cms::Lib::Modules::ModuleSet.draw :zomeki_book_library, '図書情報', 999 do |mod|
+#  ## contents
+  mod.content :books, '図書'
+
+#  ## directories
+  mod.directory :books, '図書'
+
+  ## pieces
+  mod.piece :books, '図書'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,32 @@
 ZomekiBookLibrary::Engine.routes.draw do
+  ## admin
+  mod = "zomeki_book_library"
+  scope "#{ZomekiCMS::ADMIN_URL_PREFIX}/#{mod}/c:concept" do
+    resources :content_base,
+      :controller => 'admin/content/base'
+
+    resources :content_settings, :only => [:index, :show, :edit, :update],
+      :controller => 'admin/content/settings',
+      :path       => ':content/content_settings'
+
+  #  ## contents
+    resources(:books,
+      :controller => 'admin/books',
+      :path       => ':content/books')
+
+    ## nodes
+    resources :node_books,
+      :controller => 'admin/node/books',
+      :path       => ':parent/node_books'
+
+    ## pieces
+    resources :piece_books,
+      :controller => 'admin/piece/books'
+  end
+
+
+  ## public
+  scope "_public/#{mod}" do
+    get 'node_books(/index.:format)' => 'public/node/books#index'
+  end
 end
