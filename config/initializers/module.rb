5a46e72687f131f18c939e1328a0e2435d9377a3
tao-k <tao@sitebridge.co.jp>
2017-12-20 14:47:37 +0900

    add module configuration

---
+++ config/initializers/module.rb
@@ -0,0 +1,10 @@
+Cms::Lib::Modules::ModuleSet.draw_plugin :zomeki_book_library, '図書情報：プラグイン', 999 do |mod|
+#  ## contents
+  mod.content :books, '図書：プラグイン'
+
+#  ## directories
+  mod.directory :books, '図書：プラグイン'
+
+  ## pieces
+  mod.piece :books, '図書：プラグイン'
+end
