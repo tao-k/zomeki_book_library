cca60c4ee3d4a33aa2001138e02317ad6a87a7c5
tao-k <tao@sitebridge.co.jp>
2018-01-15 14:19:20 +0900

    fix base controller

--- app/controllers/zomeki_book_library/admin/content/base_controller.rb
+++ app/controllers/zomeki_book_library/admin/content/base_controller.rb
@@ -1,7 +1,5 @@
 # Controller For Managing Content Data
-
-class ZomekiBookLibrary::Admin::Content::BaseController < Cms::Admin::Node::BaseController
-  layout  'admin/cms'
+class ZomekiBookLibrary::Admin::Content::BaseController < Cms::Admin::Content::BaseController
   def model
     ZomekiBookLibrary::Content::Book
   end
